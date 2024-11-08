d06762a86f4d663f432f3acfc8d8d49aeed5f139
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-08 14:32:32 +0200

    new login html js files

---
+++ php/scripts/login.js
@@ -0,0 +1,8 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const errorDiv = document.getElementById('error');
+
+  
+});
